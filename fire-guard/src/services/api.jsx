6512edeeba107320c11a06485499ca28a548529c
marianarodokas-dotcom <mariana.rodokas@aluno.senai.br>
2026-09-08 10:55:49 -0300

    .

--- fire-guard/src/services/api.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-//ip Felipe Python
-const IP_PYTHON = '192.168.1.80'; 
-
-export const api = axios.create({
-  baseURL: `http://${IP_PYTHON}:8000/api`,
-  timeout: 5000,
-});
